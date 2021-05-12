bb488da27b136d827d7972bd886bc8a617610ba7
Luis <lm.perezpacheco@gmail.com>
2021-05-12 19:14:06 +0200

    feat: add caracola

---
+++ src/js/Constants.js
@@ -0,0 +1,18 @@
+const relations = {
+    cofreGaviota: {
+        total: {
+            lona: -17,
+            enrolle: -15.5,
+            caracola: -16.5,
+            vuelo: -16.5
+        },
+        lona: {
+            total: 17,
+            enrolle: 1.5,
+            caracola: 0.5,
+            vuelo: 0.5
+        }
+    }
+
+
+}
